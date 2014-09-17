35622e86ca0a358409c16bcae294e50e1fcfd71d
Kris Mulyandani Putra <kris.mp7777@gmail.com>
2014-09-17 20:44:19 +0700

    edit application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,20 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+
+# Pick the frameworks you want:
+    require "active_record/railtie"
+    require "action_controller/railtie"
+    require "action_mailer/railtie"
+    require "sprockets/railtie"
+    # require "rails/test_unit/railtie"
+
+    # Assets should be precompiled for production (so we don't need the gems loaded then)
+    Bundler.require(*Rails.groups(assets: %w(development test)))
 
 module SampleApp
   class Application < Rails::Application
